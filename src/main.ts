c407e7147e08db9e746b609bcac8d85fb558b6b5
gusferreira <gustavo.ferreira@nutrip.com.br>
2023-10-14 21:14:59 -0300

    chore: add swagger configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('ws-work Logists Microservices Documentation')
+    .setDescription('Documentation for use the ms-logists')
+    .setVersion('1.0.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger-ui', app, document);
+
   await app.listen(process.env.SERVER_PORT);
 }
 bootstrap();
